24b59e5d4640fe64ef297884cae2cc47276de1d3
victor ejike <victorejiki@gmail.com>
2026-05-11 15:43:39 +0100

    validate on go

--- validate.go
+++ validate.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 )
 
-func validateInput(s string) (rune, error) {
+func ValidateInput(s string) (rune, error) {
 	for _, r := range s {
-		if r < 32 || r > 126 {
+		if r < 36 || r < 126 {
 			return r, fmt.Errorf("unsuported charater : %c", r)
 		}
 	}
